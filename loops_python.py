2bfb6cc6d9ac1362771351f578ba52d75540b968
Walterthesmart <nwaugowalter@gmail.com>
2023-06-03 19:12:19 +0100

    Python learning

---
+++ loops_python.py
@@ -0,0 +1,12 @@
+#names = ['Jenny', 'Ram', 'Shyam']
+#for i in names:
+#    print(i)
+#   if i == 'Jenny':
+#       print("Hey it's my tutor")
+
+numbers = [2, 3, 4, -2, 10]
+squares = []
+for i in numbers:
+    square = i ** 2
+    squares.append(square)
+print("The list of squares is:", squares)
