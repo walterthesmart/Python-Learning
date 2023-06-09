a4b6535443ff7582d45e29824af208ab26a67d7c
Walterthesmart <nwaugowalter@gmail.com>
2023-06-09 14:41:12 +0100

    whilee loop- if u don't know in advance the number of iterations

---
+++ continue.py
@@ -0,0 +1,15 @@
+#count = 1
+#while count <= 10:
+#    print(count)
+#    count += 1
+#    if count == 7:
+#        continue
+#    print("HI")
+#print("Out of loop")
+
+
+for i in range(1, 11):
+    if i == 7:
+        continue
+    else:
+        print(i)
