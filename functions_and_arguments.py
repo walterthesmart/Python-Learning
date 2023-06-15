32222047668bfddad893c715111fef1c7cea28cd
Walterthesmart <nwaugowalter@gmail.com>
2023-06-15 21:45:42 +0100

    Functions

---
+++ functions_and_arguments.py
@@ -0,0 +1,11 @@
+#def greet(name):
+#    print(f"Hi {name}")
+#    print("Are you from computer science department?")
+#greet("Jenny")
+
+def add(a,b):
+    c = a + b
+    print(f"Sum is {c}")
+add(2,9)
+add(6,9)
+add(3,6)
